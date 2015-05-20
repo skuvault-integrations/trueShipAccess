84640eeb9085b680fcffa973d786f52491cd7918
timothyj85 <timothy.jannace@skuvault.com>
2015-05-19 21:21:47 -0400

    Version .1.0.0

---
+++ TrueShip/Models/ts_client_config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrueShipConfiguration
+{
+    public class tsConfiguration
+    {
+        public string BEARERTOKEN { get; set; }
+        public DateTime LASTORDERSYNC { get; set; }
+        public DateTime LASTLOCATIONSYNC { get; set; }
+        public int COMPANYID { get; set; }
+    }
+}
